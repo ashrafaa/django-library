e18ef464e25b8fc9e9abc464a731ebeeaf10d365
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2019-02-18 15:50:19 +0000

    Add permission check on views and templates

--- catalog/views.py
+++ catalog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views.generic import ListView, DetailView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 @login_required
 def index(request):
@@ -57,3 +58,22 @@ class AuthorListView(ListView):
 
 class AuthorDetailView(DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, ListView):
+    """ Generic class based view listing books on loan to current user """
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+class LoanedBooksAllListView(PermissionRequiredMixin, ListView):
+    """ Generic class based view listing all books on loan. Only visible to users with can_mark_returned permission """
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
