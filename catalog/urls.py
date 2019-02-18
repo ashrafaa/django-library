dcbdab450bda804a07c1f14ec448ed4e8d983dcb
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2019-02-18 16:51:55 +0000

    Add renew option using form

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     re_path(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('allbooks/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
