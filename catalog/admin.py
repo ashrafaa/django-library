78f1f7359429bbc2c80bc6c718a4dbe087be6ab8
Ashraf Anwar <ashraf.anwar-apandi@wsp.com>
2019-02-18 10:56:20 +0000

    Initial version - with models and part of admin

---
+++ catalog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from catalog.models import Author, Genre, Book, BookInstance
+
+admin.site.register(Genre)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+admin.site.register(Author, AuthorAdmin)
